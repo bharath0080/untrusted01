531e9d93f69037741e815b517b55782b6f3a88b8
root <root@sealab-10.242.138.114.cts.com>
2018-03-13 16:19:11 +0530

    Test
    Change-Id: I49f770f1d3585faf285c70f57a66fffcbab85460

--- resources/eu/inuits/python.sh
+++ resources/eu/inuits/python.sh
@@ -1,1 +1,1 @@
-find /var/tmp/JENKINS/workspace/python-projects/puppetboard/ -name '*.py' -print0 | xargs -0 -L 1 python2 -m py_compile 
+find /var/tmp/JENKINS/workspace/python-projects/puppetboard/ -name '*.py' -print0 | xargs -0 -L 1 python -m py_compile 
