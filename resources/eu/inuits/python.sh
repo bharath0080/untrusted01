2fb759e5cfd96dddef6d11b8ffdaceb68c29d467
root <root@sealab-10.242.138.114.cts.com>
2018-03-13 16:17:30 +0530

    Test
    Change-Id: I2f1262a69e223803bbd4c054484896aaed57a2fd

--- resources/eu/inuits/python.sh
+++ resources/eu/inuits/python.sh
@@ -1,3 +1,1 @@
-echo " executing in `pwd`"
-echo -e "\n"
-find . -name '*.py' -print0 | xargs -0 -L 1 python2 -m py_compile 
+find /var/tmp/JENKINS/workspace/python-projects/puppetboard/ -name '*.py' -print0 | xargs -0 -L 1 python2 -m py_compile 
