0a0636fb4a1253d672b23497da59f0654d70f625
root <root@sealab-10.242.138.114.cts.com>
2018-03-14 10:18:10 +0530

    Test
    Change-Id: Iee77797a0eb7de01f280a4335d2517e410b59a97

--- resources/eu/inuits/python.sh
+++ resources/eu/inuits/python.sh
@@ -1,4 +1,4 @@
 files=`ls -ltr`
 echo $files
-compile=`find . -name '*.py' -print0| xargs -0 -L 1 python2 -m py_compile`
+compile=`find . -name '*.py' -print0| xargs -0 -t -L 1 python2 -m py_compile`
 echo $compile
