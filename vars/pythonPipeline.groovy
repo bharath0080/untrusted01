db1d687737e182a29d96875d30f69a8d0a8bbe11
root <root@sealab-10.242.138.114.cts.com>
2018-03-14 15:16:00 +0530

    Test
    Change-Id: Icad47e3fd8df2aab0ef40ad32a68deae302017dc

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -17,26 +17,24 @@
 //		}
 //	}
 //}
-def call(body){
-  def config = [:]
-  body.resloveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
-  pipeline{
-  	agent{
-		label "LINUX"
-	}
-	stages{
-		stage("Clone"){
-			steps{
-				git url: "https://github.com/${config.githubproject}"
+def call(body) {
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+	pipeline {
+		agent { label "LINUX" }
+		stages {
+			stage('Clone') {
+				steps {
+					git url: "https://github.com/${config.githubproject}"
+				}
+			}
+			stage('Compile') {
+				steps{
+					sh("ls")
+				}
 			}
 		}
-		stage("Compile"){
-			steps{
-				sh "ls"
-			}
-		}
 	}
-  }
 }
