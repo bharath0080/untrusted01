3fdf0c26ac6915f0277781839b6548bf0fba2fb1
root <root@sealab-10.242.138.114.cts.com>
2018-03-14 15:35:41 +0530

    Test
    Change-Id: Id6e4bdfc1cab7128d0525589e43da14a3e5c1d26

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,13 +1,13 @@
-#def call( String directory = '.' ) {
-#	if (fileExists("requirements.txt")){
-#		sh "virtualenv venv"
-#		sh "venv/bin/pip install -r requirements.txt"
-#	}
-#	sh "echo ${directory}"
-#	sh """ find ${directory}/ -name '*.py' -print0 | xargs -0 -t -L 1 python2 -m py_compile """
-#}
+//def call( String directory = '.' ) {
+//	if (fileExists("requirements.txt")){
+//		sh "virtualenv venv"
+//		sh "venv/bin/pip install -r requirements.txt"
+//	}
+//	sh "echo ${directory}"
+//	sh """ find ${directory}/ -name '*.py' -print0 | xargs -0 -t -L 1 python2 -m py_compile """
+//}
 import static eu.inuits.PythonCompiler.*
 def call(String directory = '.'){
-    echo("Compiling ${direcotry}")
+    echo("Compiling ${directory}")
     compileDirectory(this, directory)
 }
