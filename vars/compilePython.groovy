5a06f050cd0508cd38cc51f0ad397afb8d955306
root <root@sealab-10.242.138.114.cts.com>
2018-03-14 12:07:45 +0530

    Test
    Change-Id: I1d9a3b4011e87cb2854ab8e040850081056a0be0

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -4,5 +4,5 @@ def call( String directory = '.' ) {
 		sh "venv/bin/pip install -r requirements.txt"
 	}
 	sh "echo ${directory}"
-	sh """ find ${directory} -name '*.py' -print0 | xargs -0 -t -L 1 python2 -m py_compile """
+	sh """ find ${directory}/ -name '*.py' -print0 | xargs -0 -t -L 1 python2 -m py_compile """
 }
