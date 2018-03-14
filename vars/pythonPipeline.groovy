7173457b5b8776cbd031af0e79bab705b57dc0d5
root <root@sealab-10.242.138.114.cts.com>
2018-03-14 15:05:57 +0530

    Test
    Change-Id: I14a014a68343cf92ae0337aeb1a3e9041263c64f

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -1,3 +1,4 @@
+/*
 def call(String githubproject){
 	pipeline{
 		agent {
@@ -16,4 +17,28 @@ def call(String githubproject){
 			}
 		}
 	}
+}
+*/
+def call(body){
+  def config = [:]
+  body.resloveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  pipeline{
+  	agent{
+		label "LINUX"
+	}
+	stages{
+		stage("Clone"){
+			steps{
+				git url: "https://github.com/${config.githubproject}"
+			}
+		}
+		stage("Compile"){
+			steps{
+				sh "ls"
+			}
+		}
+	}
+}
 }
